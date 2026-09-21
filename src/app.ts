31b153d5edede6c9deaaf830eb41367ffb1b2335
husnearaa <hosnearapopy462@gmail.com>
2026-09-21 17:57:51 +0600

    feat: add Zod validation for user input in /zod endpoint and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, {
 	type Application,
+	NextFunction,
 	type Request,
 	type Response,
 } from "express";
 import httpStatus from "http-status";
+import z from "zod";
 import config from "./app/config";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFound } from "./app/middleware/notFound";
@@ -28,6 +30,41 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", AuthRoutes);
+
+app.post("/zod", async (req: Request, res: Response, next : NextFunction) => {
+
+	try {
+		const UserZodSchema = z.object({
+			name: z.string().endsWith("r"),
+			email : z.email(),
+			age: z.number().optional(),
+			isVerified: z.boolean().optional(),
+			books: z.array(z.string()).optional()
+		})
+
+
+		const payload = req.body;
+
+		const result = UserZodSchema.safeParse(payload)
+
+		if(!result.success){
+			console.log(result.error);
+		}
+		if(result.success){
+			console.log(result.data);
+		}
+
+
+		res.status(httpStatus.OK).json({
+			success: true,
+			message: "Welcome to PH Healthcare System Backend",
+			data : result
+		});
+	} catch (error) {
+		console.log(error);
+		next(error)
+	}
+})
 
 // Basic route
 app.get("/", async (req: Request, res: Response) => {
